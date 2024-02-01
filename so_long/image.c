0c91aedf06b859b39cabb9cd1e322a0c2cb55983
root <root@Clerk>
2024-02-01 16:45:04 +0000

    done

---
+++ so_long/image.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   image.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 09:57:11 by root              #+#    #+#             */
+/*   Updated: 2024/01/29 09:50:04 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	image_inicialize(t_main *main)
+{
+	int	i;
+
+	main->picture.collectable = mlx_xpm_file_to_image(main->mlx,
+			"./imagens/coletavel.xpm", &i, &i);
+	main->picture.wall = mlx_xpm_file_to_image(main->mlx, "./imagens/wall.xpm",
+			&i, &i);
+	main->picture.player = mlx_xpm_file_to_image(main->mlx,
+			"./imagens/player.xpm", &i, &i);
+	main->picture.player2 = mlx_xpm_file_to_image(main->mlx,
+			"./imagens/player2.xpm", &i, &i);
+	main->picture.floor = mlx_xpm_file_to_image(main->mlx, "./imagens/chao.xpm",
+			&i, &i);
+	main->picture.exit = mlx_xpm_file_to_image(main->mlx, "./imagens/exit.xpm",
+			&i, &i);
+	main->picture.exit2 = mlx_xpm_file_to_image(main->mlx, "./imagens/exit2.xpm",
+			&i, &i);
+	main->picture.enemy = mlx_xpm_file_to_image(main->mlx, "./imagens/enemy.xpm",
+			&i, &i);
+	main->picture.enemy2 = mlx_xpm_file_to_image(main->mlx, "./imagens/enemy2.xpm",
+			&i, &i);
+}
+
+void	util_image(t_main *main, int x, int y)
+{
+	if (main->map[y][x] == '1')
+		mlx_put_image_to_window(main->mlx, main->win, main->picture.wall, (x
+				* 64), (y * 64));
+	else if (main->map[y][x] == 'P' || main->map[y][x] == '0')
+		mlx_put_image_to_window(main->mlx, main->win, main->picture.floor, (x
+				* 64), (y * 64));
+	else if (main->map[y][x] == 'E')
+	{
+		if (main->change == 1)
+			mlx_put_image_to_window(main->mlx, main->win, main->picture.exit, (x
+					* 64), (y * 64));
+		else
+			mlx_put_image_to_window(main->mlx, main->win, main->picture.exit2, (x
+					* 64), (y * 64));
+	}
+	else if (main->map[y][x] == 'C')
+		mlx_put_image_to_window(main->mlx, main->win, main->picture.collectable,
+			(x * 64), (y * 64));
+	else if (main->map[y][x] == 'X')
+	{
+		if (main->change == 3)
+			mlx_put_image_to_window(main->mlx, main->win, main->picture.enemy, (x
+					* 64), (y * 64));
+		else
+			mlx_put_image_to_window(main->mlx, main->win, main->picture.enemy2, (x
+					* 64), (y * 64));
+	}
+}
+
+int	render_image(t_main *main)
+{
+	int	x;
+	int	y;
+
+	y = -1;
+	while (main->map[++y] != NULL)
+	{
+		x = -1;
+		while (main->map[y][++x] != '\0')
+			util_image(main, x, y);
+	}
+	if(main->change == 1)
+		mlx_put_image_to_window(main->mlx, main->win, main->picture.player,
+			(main->player.x * 64), (main->player.y * 64));
+	else if(main->change == 0)
+		mlx_put_image_to_window(main->mlx, main->win, main->picture.player2,
+			(main->player.x * 64), (main->player.y * 64));
+	return (0);
+}
---
+++ so_long/so_long.h
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 09:53:39 by root              #+#    #+#             */
+/*   Updated: 2024/02/01 16:25:25 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "libft/get_next_line_bonus.h"
+# include "libft/libft.h"
+# include "minilibx-linux/mlx.h"
+# include "minilibx-linux/mlx_int.h"
+# include "printf/ft_printf.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//////////// STRUCTS  //////////////
+typedef struct s_player
+{
+	int			x;
+	int			y;
+}				t_player;
+
+typedef struct s_picture
+{
+	void		*collectable;
+	void		*wall;
+	void		*player;
+	void		*player2;
+	void		*floor;
+	void		*exit;
+	void		*exit2;
+	void		*enemy;
+	void		*enemy2;
+}				t_picture;
+
+typedef struct s_main
+{
+	void		*win;
+	char		**map;
+	int			larg;
+	int			altura;
+	void		*mlx;
+	int			moves_counter;
+	int			col;
+	int			change;
+	t_player	player;
+	t_picture	picture;
+}				t_main;
+
+///////////// VALIDATION //////////
+
+void			map_paredes(t_main *main);
+void			map_rectangle(t_main *main);
+void			map_caracteres(t_main *main);
+void			map_validate(t_main *main);
+////////// MAPS ////////////
+
+int				map_caracteres_valids(char *s);
+char			**save_map(char **map, int fd);
+void			check_map(char **av, t_main *main);
+
+///////////// CONTROLS ////////////
+int				key_hook(int keycode, t_main *main);
+int				read_esc(int keycode, t_main *main);
+void			verification_moves(t_main *m, int x, int y);
+int				ft_strlen_sl(const char *s);
+
+////////////// IMAGENS //////////////
+void			image_inicialize(t_main *main);
+int				render_image(t_main *main);
+void			util_image(t_main *main, int x, int y);
+
+
+///////////  MAIN //////////
+int				end(t_main *main);
+void			free_map(char **m);
+
+////////// ALGORITMO  /////////////
+int				algoritmo(int x, int y, char **map_copy, int col, t_main *main);
+char			**copy_map(t_main *main);
+void			check_map_alg(t_main *main, char **copy_map_temp);
+
+#endif
